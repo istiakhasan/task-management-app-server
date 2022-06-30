78ac7558c5744a21a24737fcb43bc0e1ba529943
Istiak <istieak.hasan1996@gmail.com>
2022-06-30 21:05:28 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express=require('express')
 const app=express()
+const cors=require('cors')
+app.use(cors())
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 4000
 
-
-
-
-
 app.get('/',(req,res)=>{
-    res.send({name:"korim",roll:"220"})
+    res.send("Started app successfully")
 })
+
+
+//user:todoadmin
+//passwsord:ULrZLvHNeWv4XX71
+
+
+
+const uri = "mongodb+srv://todoadmin:ULrZLvHNeWv4XX71@cluster0.l0msl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run=async()=>{
+   try{
+   await client.connect();
+   const todoCollections=client.db('todo').collection('todolist');
+   todoCollections.insertOne({name:"robin"})
+
+   }finally{}
+}
+
+run().catch(console.dir)
+
 
 
 
